6ab1399ea20cb548fce8319cacce5ca395dc76dc
Tobias Willig <willig.tobias@gmail.com>
2023-07-23 11:52:54 +0200

    fix: renamed serverless-terraform to lambdaform

--- libs/lambdaform/jest.config.ts
+++ libs/lambdaform/jest.config.ts
@@ -1,10 +1,10 @@
 /* eslint-disable */
 export default {
-  displayName: 'serverless-terraform',
+  displayName: 'lambdaform',
   preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/libs/serverless-terraform',
+  coverageDirectory: '../../coverage/libs/lambdaform',
 };
