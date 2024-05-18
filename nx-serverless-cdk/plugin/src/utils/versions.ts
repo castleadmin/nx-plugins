d460183c3d3c81b9302ce4ea9e9e3696ea806cd3
Tobias Willig <willig.tobias@gmail.com>
2024-05-18 11:18:55 +0200

    feat: upgraded to nx 19

--- nx-serverless-cdk/plugin/src/utils/versions.ts
+++ nx-serverless-cdk/plugin/src/utils/versions.ts
@@ -27,29 +27,29 @@ export interface Versions {
 
 export const getVersions = (): Versions => {
   // CDK dependencies
-  const awsCdkLib = '^2.133.0';
+  const awsCdkLib = '^2.142.1';
   const constructs = '^10.3.0';
   const sourceMapSupport = '^0.5.21';
   // CDK development dependencies
-  const awsCdk = '^2.133.0';
+  const awsCdk = '^2.142.1';
   const tsconfigPaths = '^4.2.0';
   const tsNode = '^10.9.2';
   // Lambda dependencies
-  const powertoolsLogger = '^2.0.3';
+  const powertoolsLogger = '^2.1.1';
   // Lambda development dependencies
-  const typesAwsLambda = '^8.10.136';
+  const typesAwsLambda = '^8.10.138';
   const esbuild = '^0.19.12';
   // TypeScript dependencies
   const tslib = '^2.6.2';
   // TypeScript development dependencies
-  const typesNode = '^20.11.30';
+  const typesNode = '^20.12.12';
   // E2E common
-  const awsSdkCredentialProviders = '^3.540.0';
-  const awsSdkClientSsm = '^3.540.0';
+  const awsSdkCredentialProviders = '^3.577.0';
+  const awsSdkClientSsm = '^3.577.0';
   // E2E generic dependencies
-  const awsSdkClientSqs = '^3.540.0';
+  const awsSdkClientSqs = '^3.577.0';
   // E2E lambda dependencies
-  const awsSdkClientLambda = '^3.540.0';
+  const awsSdkClientLambda = '^3.577.0';
 
   return {
     // CDK dependencies
